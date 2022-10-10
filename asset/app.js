4f1d09c936fe165f68de6ab6bd0f9c1c6bf59470
Ferry Ayunda <114124884+susahberak@users.noreply.github.com>
2022-10-10 14:49:53 +0700

    Update app.js

--- asset/app.js
+++ asset/app.js
@@ -21,14 +21,14 @@ const getDownloadLink = async () => {
     $('#result').show()
 
     $('#title').html(res.title)
-    $('#source').html(`<a class='text-white' href='${vid_url}'>${vid_url}</a>`)
+    $('#source').html(`<a class='text-decoration-none' href='${vid_url}'>${vid_url}</a>`)
 
     $('#links').html('')
 
     const links = res.links
 
     links !== undefined && Object.keys(links).forEach(function (key) {
-      $('#links').append(`<a class="btn btn-info mr-2" href="${links[key]}&dl=1" download="${key}.mp4">${key}</a>`)
+      $('#links').append(`<a class="btn btn-primary" href="${links[key]}&dl=1" download="${key}.mp4">${key}</a>`)
     })
   } else {
     $('#bar').hide()
